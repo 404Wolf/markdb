00d469e6065cc7376b99c20583cfab2d6ead704d
Wolf Mermelstein <wolf@404wolf.com>
2025-11-16 22:26:54 -0500

    init

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "Typescript-Node DevShell";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }@inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              bun
+              prettierd
+            ];
+          };
+        };
+      }
+    );
+}
